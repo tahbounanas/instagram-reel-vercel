4e26f86aea4fcf09256862fe3075c53a4df365df
tahbounanasse <tahbounanasse@gmail.com>
2025-06-29 02:38:55 +0100

    Final working version for Vercel Python

--- api/reel.py
+++ api/reel.py
@@ -3,21 +3,25 @@ import json
 
 def handler(request):
     try:
-        query = request.get("query", {})
-        url = query.get("url", "")
+        url = request.get("query", {}).get("url")
         if not url:
             return {"statusCode": 400, "body": json.dumps({"error": "Missing URL"})}
 
-        shortcode = url.rstrip('/').split('/')[-1]
+        shortcode = url.rstrip("/").split("/")[-1]
         L = Instaloader()
         post = Post.from_shortcode(L.context, shortcode)
 
-        data = {
-            "user": post.owner_username,
-            "video_url": post.video_url,
-            "caption": post.caption or "Enjoy this reel"
-        }
-        return {"statusCode": 200, "body": json.dumps(data)}
+        user = post.owner_username
+        video_url = post.video_url
+        caption = (post.caption or "Enjoy this reel").replace("\n", " ").replace(";", ",")
 
+        return {
+            "statusCode": 200,
+            "body": json.dumps({
+                "user": user,
+                "video_url": video_url,
+                "caption": caption
+            })
+        }
     except Exception as e:
         return {"statusCode": 500, "body": json.dumps({"error": str(e)})}
